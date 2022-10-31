618e0070d5680434dddb25ea7aed5b8c8400fa7e
Asiya Farseen <farseen.a@flatworldlocal.com>
2022-10-31 15:59:59 +0530

    first commit

---
+++ backendAPI/backendAPI/Data/FullStackDbContext.cs
@@ -0,0 +1,13 @@
+﻿using backendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendAPI.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ frontEnd/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddEmployeeComponent,
+    EmployeesListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
